2239ac491d73e00d969757615767ec7f1cf71a34
Piotr Bełda <piotrek.belda@wp.pl>
2026-02-09 08:50:57 +0100

    test data insert

--- test/test.sh
+++ test/test.sh
@@ -3,11 +3,11 @@
 ogr2ogr \
 -f "PostgreSQL" \
 PG:"service=db" \
-WFS:"https://wody.isok.gov.pl/wss/INSPIRE/INSPIRE_HY_10k_2019_WFS" \
-hy-p:Watercourse \
--nln public.watercourse
+WFS:"https://wody.isok.gov.pl/wss/INSPIRE/INSPIRE_NZ_HY_MZPMRP_WFS" \
+"nz-core:RiskZone" \
+-nln inspire.risk_zone \
 -overwrite \
 -t_srs EPSG:2180 \
--spat 573050 5709050 578050 5714050 \
 -lco GEOMETRY_NAME=geom \
--lco FID=id
+-oo FILTER="<Filter xmlns='http://www.opengis.net/ogc'><BBOX><PropertyName>geometry</PropertyName><Envelope srsName='EPSG:2180'><lowerCorner>573050 5709050</lowerCorner><upperCorner>578050 5714050</upperCorner></Envelope></BBOX></Filter>" \
+-skipfailures
